0c8737d7e8728f7b493761d4ca517469433ee9a4
Konner Hartman <konner.hartman@yahoo.com>
2021-09-17 19:58:18 -0500

    Added note to js with APIs that we are using.

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
+// API links 
+// cocktails - www.thecocktaildb.com/api/json/v1/1/search.php?s=
+// movies - http://www.omdbapi.com/?apikey=90c34782&t=
+
 var $spirits = document.getElementById("#userSpirits")
 var $whiskey = document.getElementById("#userWhiskey")
 var $vodka = document.getElementById("#userVodka")
