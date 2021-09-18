9cc18e426cf0bbdc7a4ed39cd728128d75f0f15a
Thu Phan <alicebe.phan@gmail.com>
2021-09-18 10:51:55 -0500

    worked a bit on logic

--- assets/script.js
+++ assets/script.js
@@ -8,15 +8,47 @@ var $vodka = document.getElementById("#userVodka")
 var $gin = document.getElementById("#userGin")
 var $rum = document.getElementById("#userRum")
 var $tequila = document.getElementById("#userTequila")
-
 $(document).ready(function(){
-    $('select').formSelect();
+  $('select').formSelect();
+});
+
+fetch('http://www.omdbapi.com/?apikey=90c34782&t=star+wars', {
+  method: 'GET', 
+  credentials: 'same-origin', 
+  redirect: 'follow', 
+})
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
   });
 
-function userSpirit () {
-    if ($spirits.value == "whiskey") {
-        $whiskey.removeClass("#hide")
-    }
+  fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=vodka', {
+    method: 'GET', 
+    credentials: 'same-origin', 
+    redirect: 'follow', 
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+  
+
+
+
+  
+
+
+  // fetch request gets drinks's data
+
+/*function userSpirit() {
+  if ($spirits.value == "whiskey") {
+    $whiskey.removeClass("#hide")
+  }
 }
 
 userSpirit();
+document.addEventListener('click',)*/
