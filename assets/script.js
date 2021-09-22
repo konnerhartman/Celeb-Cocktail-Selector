fce296b8ef8ed5675e437abf46350341a42a2f1e
Aubrey Ross <aubrey.ross04@gmail.com>
2021-09-22 15:53:23 -0500

    minor changes since lat commit. Worked on updates with Alice that she committed and will pull

--- assets/script.js
+++ assets/script.js
@@ -15,28 +15,28 @@ var contentBox = document.getElementById("content")
 });*/
 
 
-// Fetches movie
-function fetchDataMovie() {
-  fetch('http://www.omdbapi.com/?apikey=90c34782&t=casino+royale', {
-    method: 'GET',
-    credentials: 'same-origin',
-    redirect: 'follow',
-  })
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      console.log(data);
+// // Fetches movie
+// function fetchDataMovie() {
+//   fetch('http://www.omdbapi.com/?apikey=90c34782&t=casino+royale', {
+//     method: 'GET',
+//     credentials: 'same-origin',
+//     redirect: 'follow',
+//   })
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (data) {
+//       console.log(data);
       
-  });
-}
-fetchDataMovie();
+//   });
+// }
+// fetchDataMovie();
 
 // Fetches cocktail
 function fetchData() {
   console.log($(this).children('a').text());
   var drink = $(this).children('a').text();
-  fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s='+ drink, {
+  fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php" {
     method: 'GET',
     credentials: 'same-origin',
     redirect: 'follow',
@@ -47,7 +47,12 @@ function fetchData() {
     })
     .then(function (data) {
       console.log(data);
+    
+    var drink = data.drinks[0];
+    var drinkDiv = document.getElementById("drink");
 
+    //renders name of drink to the page//
+  
     document.getElementById("drinkContent").innerHTML = data;
     showDrinkContent = content => {
     var contentDiv = document.getElementById("drinkContent");
@@ -59,7 +64,46 @@ function fetchData() {
         contentDiv.append(drinkEl);
         });
       }
-    
+      function displaydrink(data) {
+ 
+        var drink = data.drinks[0];
+        var drinkDiv = document.getElementById("drink");    
+       
+        // renders the name of the drink
+        var drinkName = drink.strDrink;
+        var h3 = document.createElement("h3");
+        drinkDiv.appendChild(h3);
+       
+        // renders the image of the drink
+        var drinkImg = document.createElement("img");
+        drinkImg.src = drink.strDrinkThumb;
+        drinkDiv.appendChild(drinkImg);
+
+       
+        // renders the ingredients of the drink 
+        var drinkIngredients = document.createElement("ul");
+        drinkDiv.appendChild(drinkIngredients);
+       
+        var getIngredients = Object.keys(drink)
+          .filter(function (ingredient) {
+            return ingredient.indexOf("strIngredient") == 0;
+          })
+          .reduce(function (ingredients, ingredient) {
+            if (drink[ingredient] != null) {
+              ingredients[ingredient] = drink[ingredient];
+            }
+            return ingredients;
+          }, {});
+       
+        for (let key in getIngredients) {
+          let value = getIngredients[key];
+          listItem = document.createElement("li");
+          listItem.innerHTML = value;
+          drinkIngredients.appendChild(listItem);
+        }
+       
+      }
     
   });
 }  
+
